264987601192b0803f90ac34520b3042d16b116e
Deshan <dcnm777@gmail.com>
2023-07-28 20:44:46 -0700

    made connection.js more secure for myself

--- Main/db/connection.js
+++ Main/db/connection.js
@@ -1,12 +1,11 @@
-const mysql = require("mysql2");
+require('dotenv').config();
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  // Your username
-  user: "root",
-  // Your password
-  password: "",
-  database: "employees"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect(function (err) {
--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -11,10 +11,9 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL UNSIGNED NOT NULL,
+  salary DECIMAL(10, 2) UNSIGNED CHECK (salary > 0) NOT NULL,
   department_id INT UNSIGNED NOT NULL,
-  INDEX dep_ind (department_id),
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE RESTRICT
 );
 
 CREATE TABLE employee (
@@ -22,9 +21,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT UNSIGNED NOT NULL,
-  INDEX role_ind (role_id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE RESTRICT,
   manager_id INT UNSIGNED,
-  INDEX man_ind (manager_id),
   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
